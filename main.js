0def60acc094c25b50e40652b8e29a14a9e28955
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2022-04-12 14:22:30 -0300

    Clone

---
+++ main.js
@@ -0,0 +1,17 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    
+}
